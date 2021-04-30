b449b2e6f8bf20a1a99cedd17adced1afe73262d
sarojkosabe <62433684+sarojkosabe@users.noreply.github.com>
2021-04-30 09:53:18 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+// $(document).ready(function() {
+//     $('.chat_icon').click(function() {
+//         $('.chat_box').toggleClass('active');
+//     });
+
+//     // $('.my-conv-form-wrapper').convform({selectInputStyle: 'disable'})
+// });
+// $(document).ready(function() {
+//     $("#send-btn").on("click", function() {
+//         $value = $("#data").val();
+//         $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>' + $value + '</p></div></div>';
+//         $(".form").append($msg);
+//         $("#data").val('');
+//     });
+// });
+
+
+// chatWindow = document.getElementById('chatbox');
+// var xH = chatWindow.scrollHeight;
+// chatWindow.scrollTo(0, xH);
